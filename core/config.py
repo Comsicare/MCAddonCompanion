a6a5e4af91fcf1b5ce883b929f6063642e576450
comsi <bieselhsteam@gmail.com>
2026-05-18 00:05:28 +0200

    chore: bump to 0.1.8-alpha

--- core/config.py
+++ core/config.py
@@ -47,7 +47,7 @@ BLACKLIST_DEFAULTS = [
     "instance.cfg",
 ]
 
-VERSION = "0.1.7-alpha"
+VERSION = "0.1.8-alpha"
 GITHUB_REPO = "Comsicare/MCAddonCompanion"
 GITHUB_API_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases"
 
